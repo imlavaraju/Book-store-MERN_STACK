fb3ea444723fbecf4e3fca5dd48c32996b0a96fb
LAVARAJU GORLI <141550385+imlavaraju@users.noreply.github.com>
2024-05-31 13:59:25 +0530

    Update BookSingleCard.jsx

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -6,27 +6,20 @@ import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 import { useState } from "react";
 import BookModal from "./BookModal";
+import { IoBookOutline } from "react-icons/io5";
 
 const BookSingleCard = ({ book }) => {
   const [showModal, setShowModal] = useState(false);
-
   return (
     <div
       onClick={() => setShowModal(true)}
       className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl"
     >
-      <h2 className="absolute top-1 right-2 px-4 py-1 bg-red-300 rounded-lg">
-        {book.published_year}
-      </h2>
-      <div className="flex justify-start items-center gap-x-2">
-        <PiBookOpenTextLight className="text-red-300 text-2xl" />
-        <h2 className="my-1">{book.bookname}</h2>
+      <IoBookOutline className="w-full h-24 object-cover rounded-lg" />
+      <div className="text-center mt-2">
+        <h2 className="text-lg font-semibold">{book.bookname}</h2>
+        <p className="text-gray-600">{book.author_name}</p>
       </div>
-      <div className="flex justify-start items-center gap-x-2">
-        <BiUserCircle className="text-red-300 text-2xl" />
-        <h2 className="my-1">{book.author_name}</h2>
-      </div>
-
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
